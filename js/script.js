c69b05148dea0629ec6bb3fae55d5214b5454a37
Nudyess <to.czyzewski@gmail.com>
2024-05-06 23:52:34 +0200

    added year information in the footer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const menuButton = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav-mobile')
+const footerYear = document.querySelector('.footer_year')
 
 const showMobileMenu = () => {
 	navMobile.classList.toggle('nav-mobile--active')
 	document.body.classList.toggle('lock-scroll')
 }
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
 menuButton.addEventListener('click', showMobileMenu)
