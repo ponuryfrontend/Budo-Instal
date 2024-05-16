f08cd0be3e0b0cc7ff9f6cb27fd6b9d6e8489dfa
Nudyess <to.czyzewski@gmail.com>
2024-05-16 13:00:13 +0200

    Fixed brand slider (only css animation, not JS), and added icons file

--- js/script.js
+++ js/script.js
@@ -6,12 +6,6 @@ let currentSlide = 0;
 const slides = document.querySelectorAll('.slider__slide-box');
 const totalSlides = slides.length;
 let slideInterval;
-// Brand Slider 
-let currentIndex = 0;
-let animationId;
-let sliderSpeed = 3500;
-const brandSlider = document.querySelector('.brand-slider__box');
-const brandLogos = document.querySelectorAll('.brand-slider__brand');
 
 const showMobileMenu = () => {
 	navMobile.classList.toggle('nav-mobile--active')
@@ -36,56 +30,12 @@ document.querySelector('.arrow--nextBtn').addEventListener('click', nextSlide);
 document.querySelector('.slider').addEventListener('mouseenter', stopSlideShow);
 document.querySelector('.slider').addEventListener('mouseleave', startSlideShow);
 
-// Brand slider 
-
-function slideToNext() {
-    currentIndex++;
-    if (currentIndex >= brandLogos.length) {
-        currentIndex = 0; // Wróć do pierwszego logotypu po wyświetleniu ostatniego
-    }
-    updateSliderPosition();
-}
-
-function updateSliderPosition() {
-    const newPosition = -currentIndex * (brandLogos[0].offsetWidth + 105); // Przesunięcie o szerokość logotypu + odstęp
-    const screenWidth = window.innerWidth; // Pobierz szerokość ekranu
-
-    // Ustaw czas trwania animacji w zależności od szerokości ekranu
-    let transitionDuration = '3.5s'; // Domyślny czas trwania animacji
-    if (screenWidth > 810) {
-        transitionDuration = '6s'; // Czas trwania animacji na większych ekranach
-        sliderSpeed = '6000';
-    }
-
-    brandSlider.style.transition = `transform ${transitionDuration} linear`;
-    brandSlider.style.transform = `translateX(${newPosition}px)`;
-}
-
-function startAutoSlide() {
-    stopAutoSlide();
-    animationId = setInterval(slideToNext, sliderSpeed);
-}
-
-function stopAutoSlide() {
-    clearInterval(animationId);
-}
-
 // Current Year on the footer 
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
-
-// Brand Slider - rozpoczęcie automatycznego przesuwania
-startAutoSlide();
-// Przesunięcie po wyświetleniu ostatniego logotypu
-brandSlider.addEventListener('transitionend', () => {
-    if (currentIndex === brandLogos.length) {
-        currentIndex = 0;
-        updateSliderPosition();
-    }
-});
 
 showSlide(currentSlide);
 startSlideShow();
